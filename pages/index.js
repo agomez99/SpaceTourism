dc8ff1227af4fb61fa4bcdb99467699fb2e87bf9
agomez99 <austine.gomez@yahoo.com>
2023-06-30 12:47:30 -0500

    1 file modified

--- pages/index.js
+++ pages/index.js
@@ -49,11 +49,7 @@ export default function Home() {
         </div>
       </main>
 
-      <footer className={styles.footer}>
-        <a href="https://next.new" target="_blank" rel="noopener noreferrer">
-          Created with&nbsp;<b>next.new</b>&nbsp;⚡️
-        </a>
-      </footer>
+
     </div>
   );
 }
